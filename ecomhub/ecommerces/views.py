d7df0b4ecc6afbd49802c4806503d7c862884d7e
nguyendzaii16112004@gmail.com <nguyendzaii16112004@gmail.com>
2025-05-29 14:18:56 +0700

    add history_orders api

--- ecomhub/ecommerces/views.py
+++ ecomhub/ecommerces/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.defaulttags import comment
 from django.utils.timezone import activate
-from rest_framework import filters
+from rest_framework import filters, mixins
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.generics import ListAPIView, CreateAPIView
 from rest_framework.response import Response
@@ -24,7 +24,7 @@ from rest_framework.decorators import action
 from .serializers import CategorySerializer, UserSerializer, ShopSerializer, ProductSerializer, CommentSerializer, \
     ProductImageSerializer, OrderSerializer, OrderDetailWithProductSerializer, \
     PaymentSerializer, CartSerializer, CartDetailSerializer
-from django.db.models import Sum, F, functions as db_func, Q
+from django.db.models import Sum, F, functions as db_func, Q, Avg
 from rest_framework.views import APIView
 from django.conf import settings
 from datetime import datetime
@@ -35,10 +35,11 @@ class CategoryViewSet(viewsets.ViewSet, generics.ListAPIView):
     serializer_class = CategorySerializer
 
 
-class UserViewSet(viewsets.ViewSet, generics.CreateAPIView):
+class UserViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     queryset = User.objects.filter(is_active=True)
     serializer_class = UserSerializer
     parser_classes = [parsers.MultiPartParser, ]
+    lookup_field = 'pk'
 
     @action(methods=['get', 'patch'], url_path='current-user', detail=False,
             permission_classes=[permissions.IsAuthenticated])
@@ -56,6 +57,7 @@ class UserViewSet(viewsets.ViewSet, generics.CreateAPIView):
             return Response(serializers.UserSerializer(u).data)
 
         return Response(serializers.UserSerializer(request.user).data)
+
 
 class ShopViewSet(viewsets.ViewSet, generics.ListCreateAPIView, generics.RetrieveAPIView):
     queryset = Shop.objects.filter(active=True)
@@ -182,6 +184,36 @@ class ProductViewSet(viewsets.ViewSet, generics.ListAPIView, generics.RetrieveAP
                 return p.get_paginated_response(serializer.data)
             else:
                 return Response(CommentSerializer(comments, many=True).data)
+
+    @action(methods=['get'], url_path='compare', detail=True)
+    def compare_products(self, request, pk=None):
+        current_product = self.get_object()
+
+        products = Product.objects.filter(
+            active=True,
+            category=current_product.category
+        ).exclude(pk=current_product.pk).exclude(shop=current_product.shop).prefetch_related('images', 'shop')
+
+        data = []
+        for product in products:
+            comments = product.comment_set.filter(active=True)
+            avg_star = comments.aggregate(Avg('star'))['star__avg'] or 0
+            total_comments = comments.count()
+
+            data.append({
+                'id': product.id,
+                'name': product.name,
+                'price': product.price,
+                'shop': product.shop.name,
+                'image': product.images.first().image.url if product.images.exists() else None,
+                'avg_star': round(avg_star, 1),
+                'total_comments': total_comments
+            })
+
+        # Sort theo tiêu chí: Giá tăng dần, rồi sao giảm dần
+        data = sorted(data, key=lambda x: (x['price'], -x['avg_star']))
+
+        return Response(data)
 
 
 class CommentViewSet(viewsets.ViewSet, generics.DestroyAPIView, generics.UpdateAPIView):
@@ -360,6 +392,20 @@ class OrderViewSet(viewsets.ViewSet, generics.ListAPIView, generics.RetrieveAPIV
             orderdetails = orderdetails.filter(product__name__icontains=q)
 
         return Response(OrderDetailWithProductSerializer(orderdetails, many=True).data, status=status.HTTP_200_OK)
+
+    @action(methods=['get'], detail=False, url_path='history_orders')
+    def get_history_orders(self, request):
+        user = request.user
+        orders = Order.objects.filter(user=user, status='PAID', active=True).order_by('-created_date')
+
+        page = self.paginate_queryset(orders)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(orders, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 
 class PaymentViewSet(viewsets.ViewSet, generics.ListAPIView, generics.CreateAPIView):
